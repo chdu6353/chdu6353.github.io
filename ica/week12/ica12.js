0ca841c5d0aa7dee812d6c1851b69ce89d866456
chdu6353 <chdu6353@colorado.edu>
2026-03-31 10:42:45 -0600

    showAnswer works

--- ica/week12/ica12.js
+++ ica/week12/ica12.js
@@ -2,6 +2,15 @@ let newBtn = document.querySelector('#js-new-quote');
 
 newBtn.addEventListener('click', getQuote); 
 
+let answerBtn = document.querySelector('#js-tweet').addEventListener('click', showAnswer); 
+
+ const answerText = document.querySelector('#js-answer-text');
+
+let current = {
+    question: "",
+    answer: ""
+}
+
 const endpoint = 'https://trivia.cyberwisp.com/getrandomchristmasquestion';
 
 async function getQuote() {
@@ -13,7 +22,10 @@ async function getQuote() {
         }
         const json = await response.json(); 
         console.log(json); 
-        displayQuote(json['question']); 
+        displayQuote(json['question']);
+        current.question = json["question"];
+        current.answer = json["answer"];
+        console.log(current.answer); 
     } catch (err){
         console.log(err)
         alert('Failed to fetch new quote');
@@ -24,4 +36,12 @@ function displayQuote(quote){
     const quoteText = document.querySelector
     ('#js-quote-text');
     quoteText.textContent = quote; 
+    answerText.textContent = ""; 
 }
+
+function showAnswer() {
+    answerText.textContent = current.answer;  
+}
+
+getQuote(); 
+
