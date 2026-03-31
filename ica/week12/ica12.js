1fa8c05cfc880faced9cc9801449668b0e7a354e
chdu6353 <chdu6353@colorado.edu>
2026-03-31 10:22:43 -0600

    coolness

--- ica/week12/ica12.js
+++ ica/week12/ica12.js
@@ -2,6 +2,26 @@ let newBtn = document.querySelector('#js-new-quote');
 
 newBtn.addEventListener('click', getQuote); 
 
-function getQuote() {
+const endpoint = 'https://trivia.cyberwisp.com/getrandomchristmasquestion';
+
+async function getQuote() {
     //alert("THIS WORKS"); 
+    try{
+        const response = await fetch(endpoint); 
+        if(!response.ok){
+            throw Error(response.statusText);
+        }
+        const json = await response.json(); 
+        console.log(json); 
+        displayQuote(json['question']); 
+    } catch (err){
+        console.log(err)
+        alert('Failed to fetch new quote');
+    }
+}
+
+function displayQuote(quote){
+    const quoteText = document.querySelector
+    ('#js-quote-text');
+    quoteText.textContent = quote; 
 }
