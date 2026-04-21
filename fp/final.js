ce417423920208fa04d3036ad5eaac2b56b678b3
chdu6353 <chdu6353@colorado.edu>
2026-04-21 07:52:06 -0600

    more java - timestamp 20.09 tutorial

--- fp/final.js
+++ fp/final.js
@@ -10,11 +10,60 @@ let gameActive = false;
 function generateMaze() { 
     maze = Array(MAZE_SIZE)
         .fill()
-        .map() => 
+        .map(() => 
                 Array(MAZE_SIZE)
-                    .fill() any
+                    .fill() 
                     .map(() => ({
-                        walls: {top: true, right: true, bottom: true, left: true}
+                        walls: {top: true, right: true, bottom: true, left: true},
                         visited: false, 
-                    })
+                }))
+            ); 
+            let stack = []; 
+            let current = {x: 0, y: 0 };
+            maze[0][0].visited = true; 
+
+            while(true){
+                let neighbors = [];
+                if(current.x > 0 && !maze[current.y][current.x - 1].visited){
+                    neighbors.push("left");
+                }
+                if(current.x < MAZE_SIZE - 1 && !maze[current.y][current.x - 1].visited) {
+                    neighbors.push("right");
+                }
+                  if(current.y < 0 && !maze[current.y - 1][current.x].visited) {
+                    neighbors.push("top");
+                }
+                  if(current.y < 0 && !maze[current.y - 1][current.x].visited) {
+                    neighbors.push("bottom");
+                }
+                if(neighbors.length > 0){
+                    let direction = neighbors[Math.floor(Math.random() * neighbors.length)];
+                    let next = { x: current.x, y: current.y};
+
+                    switch(direction){
+                        case "left":
+                            maze[current.y][current.x].walls.left = false; 
+                            maze[current.y][current.x - 1].walls.right = false;
+                            next.x--; 
+                            break; 
+                        case "right":
+                            maze[current.y][current.x].walls.right = false; 
+                            maze[current.y][current.x + 1].walls.left = false;
+                            next.x++; 
+                            break; 
+                        case "top":
+                            maze[current.y][current.x].walls.top = false; 
+                            maze[current.y - 1][current.x].walls.bottom = false;
+                            next.y--; 
+                            break; 
+                        case "bottom":
+                            maze[current.y][current.x].walls.bottom = false; 
+                            maze[current.y + 1][current.x].walls.top = false;
+                            next.y++; 
+                            break; 
+                    }
+
+                    maze[next.y][next.x].visited
+                }
+            }
 }
